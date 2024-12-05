456ab701457781ebaa929d5fef79cca165adc562
walternzd@gmail.com <walternzd@gmail.com>
2024-12-05 07:34:46 +0000

    Created 2024-12-05_Logo.jpg, Form2 and 2024-12-05_logo_3.jpeg; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,3 +8,15 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    chars = ["1", "2", "3", "4", "d", "c",
+            "5", "6", "7"," 8", "+", "-",
+            "9", "0", ".", "*", "/", "="  ]
+    self.btn = {}
+    gp = GridPanel()
+    for i in chars:
+      self.btn[i] = Button(text=i)
+      gp.add_component(self.btn[i], 
+                      row = 'A', 
+                      col_xs=3,
+                      width_xs=1)
+    self.add_component(gp)
